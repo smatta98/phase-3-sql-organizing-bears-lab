4e841e74326af69962cc0e92f9cc02f3be11721d
Shiv Matta <shiv.matta@gmail.com>
2023-01-10 19:56:06 -0800

    commit tree b10642da655aaef23f2b583860341a4c96606cdd

---
+++ lib/insert.sql
@@ -0,0 +1,26 @@
+--  INSERT INTO bears (id, name, age, sex, color, temprament, alive) VALUES (1,"Mr. Chocolate", 1, "M", "brown", "nice", 0),
+--    (2,"Rowdy", 2, "F", "black", "nice", 0),
+--      (3,"Tabitha", 3, "M", "brown", "nice", 0),
+--     (4,"Sergeant Brown", 4, "F", "brown", "nice", 0),
+--     (5,"Melissa", 5, "M", "white", "nice", 0),
+--     (6,"Grinch", 6, "F", "brown", "nice", 0),
+--     (7,"Wendy", 7, "M", "white", "nice", 0),
+--     (8,DEFAULT, 8, "F", "black", "nice", 0);
+
+
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
+
+
+
+ 
+
+ 
